49c15e1cdd9945abd3cbc0d4281eb8024d4e2dc3
Babuaravind Gururaj <gururaj.b@northeastern.edu>
2024-02-23 20:51:51 -0500

    actions change

--- db.sh
+++ db.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo sed -i 's/ident/md5/g' /var/lib/pgsql/data/pg_hba.conf
 sudo su - postgres <<EOF
-psql -c "CREATE USER  WITH PASSWORD 'root';"
+psql -c "CREATE USER  WITH PASSWORD '';"
 psql -c "CREATE DATABASE ;"
 psql -c "GRANT ALL PRIVILEGES ON DATABASE  TO ;"
 psql -c "ALTER USER  WITH SUPERUSER;"
