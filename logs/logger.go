0091f6b0ee8fca5ab2545ceca9e3456caaae8ba9
Babuaravind Gururaj <gururaj.b@northeastern.edu>
2024-03-20 16:19:06 -0400

    packer modification

--- logs/logger.go
+++ logs/logger.go
@@ -2,6 +2,7 @@ package logs
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"go.uber.org/zap"
@@ -12,11 +13,22 @@ import (
 func CreateLogger() *zap.Logger {
 	logFilePath := "/var/log/myapp/app.log"
 
+	_, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		// Instead of fmt.Println, use standard logging to ensure it's captured by journalctl/systemctl.
+		// For services managed by systemd, stdout and stderr are captured by journald.
+		log.Printf("Error creating/opening log file: %v\n", err)
+		// Consider how you want to handle this error. Options might include returning a nil logger, exiting, or falling back to console logging.
+		return nil
+	}
+
 	// Check if the log file or directory does not exist
 	if _, err := os.Stat(logFilePath); os.IsNotExist(err) {
 		fmt.Println("Log file is not available")
+		log.Printf("Log file is not available")
 		// Optionally, create the directory and file here if needed
 	} else {
+		log.Printf("Log file is available or an error occurred checking the file")
 		fmt.Println("Log file is available or an error occurred checking the file")
 	}
 
