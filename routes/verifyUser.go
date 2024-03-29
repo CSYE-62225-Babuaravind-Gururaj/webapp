403387a084b23344783c93ff9b39a58cc804cb71
Babuaravind Gururaj <gururaj.b@northeastern.edu>
2024-03-29 02:12:19 -0400

    endpoint fix

--- routes/verifyUser.go
+++ routes/verifyUser.go
@@ -3,52 +3,52 @@ package routes
 import (
 	"cloud-proj/health-check/database"
 	"cloud-proj/health-check/models"
-	"errors"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 )
 
 func VerifyUserRoute(c *gin.Context) {
+	// var user models.User
+	// err := database.DB.Order("id DESC").First(&user).Error
+	// if err != nil {
+	// 	log.Printf("Failed to fetch an existing user: %v", err)
+	// }
+
 	token := c.Query("token")
 
-	log.Printf("Token print: %s", token)
+	log.Printf("Token print: %s", c.Query("token"))
 
 	if token == "" {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Token query parameter is required"})
 		return
 	}
 
+	// Fetch the associated VerifyUser entry to get the token (ID in this case).
 	var verifyUser models.VerifyUser
-	err := database.DB.Where("id = ?", token).First(&verifyUser).Error
+	err := database.DB.Where("token = ?", token).First(&verifyUser).Error
 	if err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			log.Printf("VerifyUser entry not found for token: %s", token)
-			c.JSON(http.StatusNotFound, gin.H{"error": "Verification record not found"})
-			return
-		}
-		log.Printf("Failed to fetch VerifyUser entry for the token: %s, Error: %v", token, err)
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch verification record"})
-		return
+		log.Printf("Failed to fetch VerifyUser entry for the user: %v", err)
 	}
 
-	twoMinutesFromNow := time.Now().Add(-2 * time.Minute) // Subtracting time for comparison with past
-	if verifyUser.EmailTriggerTime.Before(twoMinutesFromNow) {
+	// Expiry check
+	twoMinutesFromNow := time.Now().Add(2 * time.Minute)
+	if verifyUser.EmailTriggerTime.After(twoMinutesFromNow) {
 		c.JSON(http.StatusGone, gin.H{"error": "Verification link has expired"})
 		return
 	}
 
+	// Email already verified
 	if verifyUser.EmailVerified {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Email is already verified"})
 		return
 	}
 
+	// Verification
 	verifyUser.EmailVerified = true
 	if err := database.DB.Save(&verifyUser).Error; err != nil {
-		log.Printf("Failed to update verification status for token: %s, Error: %v", token, err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update verification status"})
 		return
 	}
