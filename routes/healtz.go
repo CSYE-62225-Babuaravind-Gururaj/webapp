4c20cc24cfc1f1f53a525cd40afa6a9c2777d6f0
Babuaravind Gururaj <gururaj.b@northeastern.edu>
2024-04-10 05:50:55 -0400

    CI/CD final

--- routes/healtz.go
+++ routes/healtz.go
@@ -72,9 +72,10 @@ func RouteHealthz(db *gorm.DB) gin.HandlerFunc {
 			return
 		}
 
-		c.Header("Content-Type", "text/plain")
+		// c.Header("Content-Type", "text/plain")
 		c.Header("Cache-Control", "no-cache, no-store, must-revalidate")
-		c.String(http.StatusOK, "CI/CD check done")
+		// c.String(http.StatusOK, "CI/CD check done")
+		c.Status(http.StatusOK)
 
 		// Log the successful health check
 		logger.Info().
